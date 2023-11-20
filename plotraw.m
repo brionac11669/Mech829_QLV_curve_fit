f7d5b1febc98869dd298339b124f296de3a2a038
brionac11669 <akiizaoy@gmail.com>
2023-11-20 17:44:42 -0500

    move folder

--- plotraw.m
+++
@@ -1,9 +0,0 @@
-function plotraw(data)
-figure(1); grid on; hold on
-title('Raw data')
-plot(data.time,data.force,'.')
-xlabel('Time (s)'); ylabel('Stress (Pa)')
-yyaxis right
-plot(data.time,data.eps,'-')
-xlabel('Time (s)'); ylabel('Strain (m/m)')
-end
