0b8e0dc2edbb0069d5ecfae469e4a2316238d6fe
brionac11669 <akiizaoy@gmail.com>
2023-11-30 16:48:31 -0500

    new axes label

--- utilities/plotraw.m
+++ utilities/plotraw.m
@@ -2,7 +2,7 @@ function raw_fig=plotraw(data)
 raw_fig=figure; grid on; hold on
 title('Raw data')
 plot(data.time,data.force,'.')
-xlabel('Time (s)'); ylabel('Stress (Pa)')
+xlabel('Time (s)'); ylabel('Force (N)')
 yyaxis right
 plot(data.time,data.eps,'-')
 xlabel('Time (s)'); ylabel('Strain (m/m)')
